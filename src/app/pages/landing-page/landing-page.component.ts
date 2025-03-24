cbe017d6e4d07c010e3508bedcb6e9d2f20df421
VictorFioravanteXavier <victor.fx0910@gmail.com>
2025-03-24 09:00:09 -0300

    (Fix): arrumando um bug das imagens

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -33,17 +33,17 @@ export class LandingPageComponent implements OnInit {
 
   public funcionalidades: Array<{img: string, name: string, text: string}> = [
     {
-      img: '/assets/img_cards_landing-page/img1.svg',
+      img: 'assets/img_cards_landing-page/img1.svg',
       name: "Crie suas metas",
       text: "Adicione metas facilmente com nosso editor intuitivo"
     },
     {
-      img: '/assets/img_cards_landing-page/img2.svg',
+      img: 'assets/img_cards_landing-page/img2.svg',
       name: "Organize por categorias",
       text: "Separe suas metas por áreas da vida"
     },
     {
-      img: '/assets/img_cards_landing-page/img3.svg',
+      img: 'assets/img_cards_landing-page/img3.svg',
       name: "Acompanhe seu progresso",
       text: "Visualize e celebre suas conquistas"
     },
